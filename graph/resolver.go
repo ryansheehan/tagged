d8664b3deecd44ea7c4c98212aec0eb72070f2cf
Ryan Sheehan <rsheehan@gmail.com>
2020-05-07 16:26:53 -0500

    initial commit

---
+++ graph/resolver.go
@@ -0,0 +1,10 @@
+//go:generate go run github.com/99designs/gqlgen
+
+package graph
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+// Resolver resolves the root ql objects
+type Resolver struct{}
